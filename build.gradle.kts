ff9d807af9ae5c36b052390606b7e07d4a8872a0
nishantsharma <nishant.sharma1@tothenew.com>
2026-04-06 16:24:37 +0530

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "3.5.0.2730"
 }
 
 group = "org.example"
